f8e00a0e3adee86298d330d33ab42fd6a7d75814
pollystepanova <saraslime57@gmail.com>
2024-11-11 20:31:01 +0300

    SDA

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
 <?php
+//1 ЗАДАНИЕ
+//написать скрипт который показывает сколько часов осталось между текущим временем и твоим днем рождения
 // Устанавливаю свою дату рождения 1 октября
 $mybirthdayDay = 1; 
 $mybirthdayMonth = 10; 
-// устанавливаю текущую дату и время
+// Получаю текущую дату и время
 $currentDate = new DateTime();
 // Определяю, когда будет следующий день рождения
 $year = $currentDate->format('Y');
-$birthdayDate = new DateTime("$year-$birthdayMonth-$birthdayDay");
-
+$birthdayDate = new DateTime("$year-$mybirthdayMonth-$mybirthdayDay");
 // Если день рождения уже прошел в этом году, устанавливаем следующий год
 if ($birthdayDate < $currentDate) {
     $birthdayDate->modify('+1 year');
 }
-
-// Рассчитываем разницу между текущей датой и следующим днем рождения
+// Рассчитываю разницу между текущей датой и следующим днем рождения
 $interval = $currentDate->diff($birthdayDate);
-
-// Переводим разницу в часы
+// Перевожу разницу в часы
 $hoursRemaining = ($interval->days * 24) + $interval->h;
-
 // Выводим результат
 echo "До моего дня рождения осталось $hoursRemaining часов.";
